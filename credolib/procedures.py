190f03df57963204a22f08ffd9637452742e94d7
Andras Wacha <awacha@gmail.com>
2016-10-03 14:26:54 +0200

    Beautifying a format string

--- credolib/procedures.py
+++ credolib/procedures.py
@@ -53,7 +53,7 @@ def _collect_data_for_summarization(headers, raw, reintegrate, qrange):
             last_exception = sys.exc_info()
         if ex is None:
             print('Could not load {} 2D file for FSN {:d}. Exception: {}'.format(
-                ['processed', 'raw'][raw], head.fsn, traceback.format_exception(*last_exception)))
+                ['processed', 'raw'][raw], head.fsn, '\n'.join(traceback.format_exception(*last_exception))))
             ip.user_ns['badfsns'] = set(ip.user_ns['badfsns'])
             ip.user_ns['badfsns'].add(head.fsn)
             continue
